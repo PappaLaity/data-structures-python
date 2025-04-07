bd1f7a10ad2d65ad9e57b4363661d261d9930fe8
Pappalaity <pappalaity@gmail.com>
2025-04-07 18:19:45 +0000

    Comment Reverse function

--- data_structures/stack.py
+++ data_structures/stack.py
@@ -23,16 +23,16 @@ class Stack:
     def size(self):
         return len(self.__stack)
 
-    def reverse(self, my_string):
-        list_string = []
-        # Create a list of string and integrate to a stack letter by letter
-        for i in range(len(my_string)):
-            self.push(my_string[i])
-        while not self.isEmpty():
-            # Pop it in list and return it as a string
-            list_string.append(self.peek())
-            self.pop()
-        return "".join(list_string)
+    # def reverse(self, my_string):
+    #     list_string = []
+    #     # Create a list of string and integrate to a stack letter by letter
+    #     for i in range(len(my_string)):
+    #         self.push(my_string[i])
+    #     while not self.isEmpty():
+    #         # Pop it in list and return it as a string
+    #         list_string.append(self.peek())
+    #         self.pop()
+    #     return "".join(list_string)
 
     # def are_brackets_balanced(s):
     #     pass
